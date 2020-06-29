f98ac381fa5003101c0b4d8296f7850b44032ff7
Erica Liu <eliu925@gmail.com>
2020-06-29 00:16:44 -0400

    able to destroy cable and split balls with cable

--- src/scripts/objects/Spikes.js
+++ src/scripts/objects/Spikes.js
@@ -4,9 +4,9 @@ export default class Spikes extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, spriteKey){
     super(scene, x, y, spriteKey)
     this.scene = scene;
-
     this.scene.physics.world.enable(this)
     this.scene.add.existing(this)
+    this.scene.physics.add.existing(this)
     this.body.setAllowGravity(false);
   }
 }
--- src/scripts/scenes/FgScene.js
+++ src/scripts/scenes/FgScene.js
@@ -8,16 +8,16 @@ export default class FgScene extends Phaser.Scene {
   fpsText
   constructor() {
     super('FgScene')
+    this.destroyCable = this.destroyCable.bind(this)
+    this.throwCable = this.throwCable.bind(this)
   }
 
   create() {
     this.fpsText = new FpsText(this)
     this.player = new Player(this, 640, 400, 'player').setScale(4.3)
-    
-    this.player.setSize(18, 27, true);
-    this.cursors = this.input.keyboard.createCursorKeys()
 
-    this.cable;
+    this.player.setSize(18, 27, true)
+    this.cursors = this.input.keyboard.createCursorKeys()
 
     this.ground = new Ground(this, this.cameras.main.width / 2, 680, 'ground')
     this.ground.displayWidth = this.cameras.main.width
@@ -43,22 +43,22 @@ export default class FgScene extends Phaser.Scene {
     // change setCircle parameters to be half the width of the circle png img when changed. (the value is the diam of the circle)
     this.ballGroup.add(this.initBall)
 
-    this.createCollisions()
-    this.createAnimations()
     this.createGroups()
+    this.createAnimations()
+    this.createCollisions()
   }
 
   update(time, delta) {
     this.fpsText.update()
 
     this.player.update(this.cursors)
- 
+
     if (this.cursors.space.isDown) {
       this.throwCable()
     }
 
-    if(this.cable) {
-      this.cable.update(null, delta);
+    if (this.cable) {
+      this.cable.update(null, delta)
     }
   }
 
@@ -70,12 +70,12 @@ export default class FgScene extends Phaser.Scene {
     this.spikeGroup = this.physics.add.staticGroup({ classType: Spikes, allowGravity: false })
     this.createCeiling(0, 0)
     this.createCeiling(1844, 0)
-    // this.cables = this.physics.add.group({
-    //   classType: Cable,
-    //   maxSize: 1,
-    //   runChildUpdate: true,
-    //   allowGravity: false,
-    // })
+    this.cables = this.physics.add.group({
+      classType: Cable,
+      maxSize: 1,
+      allowGravity: false,
+      runChildUpdate: true,
+    })
   }
   createGround(x, y) {
     this.groundGroup.create(x, y, 'ground')
@@ -101,8 +101,8 @@ export default class FgScene extends Phaser.Scene {
     })
   }
 
-
   splitBall(ball) {
+    this.destroyCable()
     ball.disableBody(true, true)
     if (ball.ballSize <= 3) {
       const rightball = new Ball(this, ball.x, ball.y, false, ball, ball.ballSize + 1)
@@ -127,24 +127,27 @@ export default class FgScene extends Phaser.Scene {
   throwCable() {
     const offsetX = 2
     // let cable = this.cables.getFirstDead()
-
-    if (!this.cable) {
+    console.log(this.cables.getLength())
+    if (!this.cables.getLength()) {
       const cableX = this.player.x + (this.player.facingLeft ? -offsetX : offsetX)
       const cableY = this.player.y + 648
 
       this.cable = new Cable(this, cableX, cableY, 'cable', this.player.facingLeft).setScale(2)
-      // this.cables.add(cable)
+      this.cables.add(this.cable)
     }
+
   }
 
-  // destroyCable() {
-  //   this.cable.destroy()
-  // }
+  destroyCable() {
+
+    console.log('cable',this.cables.getChildren())
+    this.cables.getChildren()[0].destroy()
+  }
 
   createCollisions() {
     this.physics.add.collider(this.player, this.ground)
-    // this.physics.add.collider(this.cable, this.spikeGroup)
-    // this.physics.add.collider(this.ballGroup, this.cable)
+    this.physics.add.overlap(this.spikeGroup, this.cables, this.destroyCable, null, this)
+    this.physics.add.collider(this.ballGroup, this.cables, this.splitBall, null, this)
   }
 }
 // update(time, cursors, throwCable) {
